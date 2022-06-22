ed872729f289e7b41df35773dceb8dbe0dc26aa0
diamondjems016 <jennifer.mead@columbia.edu>
2022-06-22 15:35:05 -0700

    Update time_stepper.py
    Remove numpy import, correct doc strings.

--- time_stepper.py
+++ time_stepper.py
@@ -1,21 +1,24 @@
-import numpy
-
 def LeapFrog(x, v, m, dt):
-	""" Performs one leap frog step, returns updated (x,v) """
+	"""Performs one leap frog step
+		x           = particle positions
+		v           = particle velocities 
+		m           = particle masses
+		dt          = time step 
+		returns:      positions(x) and velocities(v) for the next step."""
 	x = x + dt*v
 	a = ComputeAccel(x, m)
 	v = v + dt*a
-	return (x, v)
+	return (x,v)
 
 def Euler(x, v, m, dt):
-	"""Takes a single Euler 1st order step using the same variables as rk4 step
-		values      = dependent variables
-		deriv       = derivatives 
-		start       = initial time
-		delta       = tj+1 - t_j = time step 
-		returns:      values for the next step."""
+	"""Takes a single Euler 1st order step
+		x           = particle positions
+		v           = particle velocities 
+		m           = particle masses
+		dt          = time step 
+		returns:      positions(x) and velocities(v) for the next step."""
 
-	dv = dt * ComputeAccel(x,m)
+	dv = dt * ComputeAccel(x, m)
 	dx = dt * v
 	v += dv
 	x += dx
@@ -23,8 +26,14 @@ def Euler(x, v, m, dt):
 	return (x,v)
 
 def Euler_Cromer(x, v, m, dt):
+	"""Takes a single Euler-Cromer energy conserving step
+		x           = particle positions
+		v           = particle velocities 
+		m           = particle masses
+		dt          = time step 
+		returns:      positions(x) and velocities(v) for the next step."""
 
-	dv = dt * ComputeAccel(x,m)
+	dv = dt * ComputeAccel(x, m)
 	v += dv
 	dx = dt * v
 	x += dx
