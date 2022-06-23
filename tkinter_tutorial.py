31bcd54876b1caca2d9edd98907bd625ae11bdf3
diamondjems016 <jennifer.mead@columbia.edu>
2022-06-22 22:35:08 -0700

    Update tkinter_tutorial.py
    Develop a simple calculator

--- tkinter_tutorial.py
+++ tkinter_tutorial.py
@@ -82,7 +82,7 @@ def input_box():
 	'''
 	Options:
 		width: (int) width of box
-		boarderwidth: (int) makes box boarder wider
+		borderwidth: (int) makes box boarder wider
 		fg: (str) foreground (text) color, hex codes also work
 		bg: (str) background (button) color, hex codes also work
 
@@ -105,5 +105,109 @@ def input_box():
 	# Button widget
 	myButton = tk.Button(root, text="Enter your name:", command = myClick)
 	myButton.pack()
+
+	root.mainloop()
+
+'''
+Simple Calculator App
+'''
+def calculator_ex():
+	root = tk.Tk()
+
+	# Entry widget
+	entry = tk.Entry(root, width = 35, borderwidth = 5)
+	entry.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
+
+	def button_click(number):
+		current = entry.get()
+		# Delete current box entry so numbers are not continuously appended
+		entry.delete(0,'end')
+		# Use buttons to populate entry box, for multiple digit numbers, add new click after previous clicks
+		entry.insert(0, str(current) + str(number))
+
+	def button_clear():
+		entry.delete(0,'end')
+
+	def button_add():
+		global a
+		global math
+		math = 'addition'
+		a = int(entry.get())
+		entry.delete(0,'end')
+
+	def button_subtract():
+		global a
+		global math
+		math = 'subtraction'
+		a = int(entry.get())
+		entry.delete(0,'end')
+
+	def button_multiply():
+		global a
+		global math
+		math = 'multiplication'
+		a = int(entry.get())
+		entry.delete(0,'end')
+
+	def button_divide():
+		global a
+		global math
+		math = 'division'
+		a = int(entry.get())
+		entry.delete(0,'end')
+
+	def button_equals():
+		b = int(entry.get())
+		entry.delete(0,'end')
+		if math == 'addition':
+			entry.insert(0, a+b)
+		if math == 'subtraction':
+			entry.insert(0, a-b)
+		if math == 'multiplication':
+			entry.insert(0, a*b)
+		if math == 'division':
+			entry.insert(0, a/b)
+
+
+
+	# Button widgets
+	b1 = tk.Button(root, text = "1", padx = 40, pady = 20, command = lambda: button_click(1))
+	b2 = tk.Button(root, text = "2", padx = 40, pady = 20, command = lambda: button_click(2))
+	b3 = tk.Button(root, text = "3", padx = 40, pady = 20, command = lambda: button_click(3))
+	b4 = tk.Button(root, text = "4", padx = 40, pady = 20, command = lambda: button_click(4))
+	b5 = tk.Button(root, text = "5", padx = 40, pady = 20, command = lambda: button_click(5))
+	b6 = tk.Button(root, text = "6", padx = 40, pady = 20, command = lambda: button_click(6))
+	b7 = tk.Button(root, text = "7", padx = 40, pady = 20, command = lambda: button_click(7))
+	b8 = tk.Button(root, text = "8", padx = 40, pady = 20, command = lambda: button_click(8))
+	b9 = tk.Button(root, text = "9", padx = 40, pady = 20, command = lambda: button_click(9))
+	b0 = tk.Button(root, text = "0", padx = 40, pady = 20, command = lambda: button_click(0))
+	add = tk.Button(root, text = "+", padx = 39, pady = 20, command = button_add)
+	sub = tk.Button(root, text = "-", padx = 39, pady = 20, command = button_subtract)
+	mult = tk.Button(root, text = "*", padx = 39, pady = 20, command = button_multiply)
+	div = tk.Button(root, text = "/", padx = 39, pady = 20, command = button_divide)
+	equals = tk.Button(root, text = "=", padx = 91, pady = 20, command = button_equals)
+	clear = tk.Button(root, text = "Clear", padx = 79, pady = 20, command = button_clear)
+
+	# Place buttons on grid
+	b1.grid(row = 3, column = 0)
+	b2.grid(row = 3, column = 1)
+	b3.grid(row = 3, column = 2)
+
+	b4.grid(row = 2, column = 0)
+	b5.grid(row = 2, column = 1)
+	b6.grid(row = 2, column = 2)
+
+	b7.grid(row = 1, column = 0)
+	b8.grid(row = 1, column = 1)
+	b9.grid(row = 1, column = 2)
+
+	b0.grid(row = 4, column = 0)
+	clear.grid(row = 4, column = 1, columnspan=2)
+	add.grid(row = 5, column = 0)
+	equals.grid(row = 5, column = 1, columnspan=2)
+
+	sub.grid(row = 6, column = 0)
+	mult.grid(row = 6, column = 1)
+	div.grid(row = 6, column = 2)
 
 	root.mainloop()
