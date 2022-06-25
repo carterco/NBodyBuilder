7e8695aad136f31f9e145e9eec5a808a7648a7d5
diamondjems016 <jennifer.mead@columbia.edu>
2022-06-25 00:02:37 -0700

    Changing versions...rapidly

--- NBodyBuilder/docs/conf.py
+++ NBodyBuilder/docs/conf.py
@@ -23,7 +23,7 @@ author = 'Courtney Carter, Ryan Golant, Jennifer Mead'
 root_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
